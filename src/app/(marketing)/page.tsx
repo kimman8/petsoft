e4443b94a087891dae30b1e5754178b04b4d4016
KIM YUEN <kim_man@live.com>
2024-09-17 18:59:55 +1000

    feat: home page content and styling

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,28 @@
+import Logo from '@/components/logo';
+import Image from 'next/image';
+
 export default function Home() {
-  return <main>home page</main>;
+  return (
+    <main className="bg-[#5DC9A8] min-h-screen flex items-center justify-center gap-10 flex-col xl:flex-row">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
+        alt="Preview of petsoft"
+        height={472}
+        width={519}
+      />
+      <div>
+        <Logo />
+        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+          Manage your <span className="font-extrabold">pet daycare</span> with
+          ease
+        </h1>
+        <p className="text-2xl font-medium max-w-[600px]">
+          Use Petsoft to easily keep track of pets under your care. Get Lifetime
+          access for $399.
+        </p>
+        <button>get started</button>
+        <button>login</button>
+      </div>
+    </main>
+  );
 }
