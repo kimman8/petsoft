9465b282720f216b5f8126da3ebdbbca5363a499
KIM YUEN <kim_man@live.com>
2024-09-20 16:02:11 +1000

    feat: create Pet type

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,9 +1,13 @@
+import { Pet } from '@/lib/types';
 import Image from 'next/image';
 
-export default function PetList({ pets }: { pets: any }) {
+type PetListProps = {
+  pets: Pet[];
+};
+export default function PetList({ pets }: PetListProps) {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
-      {pets.map((pet: any) => (
+      {pets.map((pet) => (
         <li key={pet.id}>
           <button className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
             <Image
