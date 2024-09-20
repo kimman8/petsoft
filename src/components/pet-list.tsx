63f7a240d00d2dff8ddaeec78ae2582116c0994c
KIM YUEN <kim_man@live.com>
2024-09-20 15:54:46 +1000

    feat: petList fetch data in a server component

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,32 +1,22 @@
 import Image from 'next/image';
 
-export default function PetList() {
+export default function PetList({ pets }: { pets: any }) {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
-      <li>
-        <button className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
-          <Image
-            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
-            alt="Pet image"
-            width={45}
-            height={45}
-            className="rounded-full object-cover"
-          />
-          <p className="font-semibold">Zeus</p>
-        </button>
-      </li>
-      <li>
-        <button className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
-          <Image
-            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
-            alt="Pet image"
-            width={45}
-            height={45}
-            className="rounded-full object-cover"
-          />
-          <p className="font-semibold">Zeus</p>
-        </button>
-      </li>
+      {pets.map((pet: any) => (
+        <li key={pet.id}>
+          <button className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+            <Image
+              src={pet.imageUrl}
+              alt="Pet image"
+              width={45}
+              height={45}
+              className="w-[45px] h-[45px] rounded-full object-cover"
+            />
+            <p className="font-semibold">{pet.name}</p>
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
