d39fa0d6e8c53143e1adb707149ef9114e8a8dff
KIM YUEN <kim_man@live.com>
2024-09-20 15:04:05 +1000

    feat: add a pet and styling

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,8 +1,31 @@
+import Image from 'next/image';
+
 export default function PetList() {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
       <li>
-        <button>Zeus</button>
+        <button className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+          <Image
+            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+            alt="Pet image"
+            width={45}
+            height={45}
+            className="rounded-full object-cover"
+          />
+          <p className="font-semibold">Zeus</p>
+        </button>
+      </li>
+      <li>
+        <button className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+          <Image
+            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+            alt="Pet image"
+            width={45}
+            height={45}
+            className="rounded-full object-cover"
+          />
+          <p className="font-semibold">Zeus</p>
+        </button>
       </li>
     </ul>
   );
