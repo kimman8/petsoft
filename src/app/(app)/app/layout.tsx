aa4702ac76f687b28c84fd8cd704f3cdc2c2247e
KIM YUEN <kim_man@live.com>
2024-09-21 14:32:34 +1000

    feat: use context provider to manage state related to pets

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -1,14 +1,28 @@
 import AppFooter from '@/components/app-footer';
 import AppHeader from '@/components/app-header';
 import BackgroundPattern from '@/components/background-pattern';
+import PetContextProvider, {
+  PetContext,
+} from '@/contexts/pet-context-provider';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const response = await fetch(
+    'https://bytegrad.com/course-assets/projects/petsoft/api/pets'
+  );
+  if (!response.ok) {
+    throw new Error('Failed to fetch pets');
+  }
+  const data = await response.json();
   return (
     <>
       <BackgroundPattern />
       <div className="mx-auto max-w-[1100px] px-4 flex flex-col min-h-screen">
         <AppHeader />
-        {children}
+        <PetContextProvider data={data}>{children}</PetContextProvider>
         <AppFooter />
       </div>
     </>
---
+++ src/contexts/pet-context-provider.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useState, createContext } from 'react';
+
+export const PetContext = createContext(null);
+
+export default function PetContextProvider({ data, children }) {
+  const [pets, setPets] = useState(data);
+  const [selectedPetId, setSelectedPetId] = useState(null);
+
+  return (
+    <PetContext.Provider
+      value={{
+        pets,
+        selectedPetId,
+      }}
+    >
+      {children}
+    </PetContext.Provider>
+  );
+}
