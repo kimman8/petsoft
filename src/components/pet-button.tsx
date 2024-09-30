5428c1e2bef76ddfd879094c7e54b42f50e3eee4
KIM YUEN <kim_man@live.com>
2024-09-30 17:07:05 +1000

    feat: add dialog(modal) for adding and editing pets

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -1,7 +1,14 @@
-'use client';
-
 import { PlusIcon } from '@radix-ui/react-icons';
 import { Button } from './ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from './ui/dialog';
+import { DialogTitle } from '@radix-ui/react-dialog';
+import PetForm from './pet-form';
+import { act } from 'react';
 
 type PetButtonProps = {
   actionType: 'add' | 'edit' | 'checkout';
@@ -13,23 +20,34 @@ export default function PetButton({
   children,
   onClick,
 }: PetButtonProps) {
-  if (actionType === 'add') {
-    return (
-      <Button size="icon">
-        <PlusIcon className="h-6 w-6" />
-      </Button>
-    );
-  } else if (actionType === 'edit') {
-    return (
-      <Button size="default" variant="secondary">
-        {children}
-      </Button>
-    );
-  } else if (actionType === 'checkout') {
+  if (actionType === 'checkout') {
     return (
       <Button size="default" variant="secondary" onClick={onClick}>
         {children}
       </Button>
     );
   }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        {actionType === 'add' ? (
+          <Button size="icon">
+            <PlusIcon className="h-6 w-6" />
+          </Button>
+        ) : (
+          <Button size="default" variant="secondary">
+            {children}
+          </Button>
+        )}
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            {actionType === 'add' ? 'Add a new pet' : 'Edit'}
+          </DialogTitle>
+        </DialogHeader>
+        <PetForm />
+      </DialogContent>
+    </Dialog>
+  );
 }
