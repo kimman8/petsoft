7ea1d6d04b161ce93fc02f70f804f1f0d1315b23
KIM YUEN <kim_man@live.com>
2024-10-05 15:33:39 +1000

    refactor: use action attribute instead of onSubmit in the pet form

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,8 +1,17 @@
 'use server';
 import prisma from '@/lib/db';
 
-export async function addPet(pet) {
+export async function addPet(formData) {
+  console.log('formData', formData);
   await prisma.pet.create({
-    data: pet,
+    data: {
+      name: formData.get('name') as string,
+      ownerName: formData.get('ownerName') as string,
+      imageUrl:
+        (formData.get('imageUrl') as string) ||
+        'https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png',
+      age: +(formData.get('age') as string),
+      notes: formData.get('notes') as string,
+    },
   });
 }
--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -5,6 +5,7 @@ import { Input } from './ui/input';
 import { Textarea } from './ui/textarea';
 import { Button } from './ui/button';
 import { usePetContext } from '@/lib/hooks';
+import { addPet } from '@/actions/actions';
 
 type PetFormProps = {
   actionType: 'add' | 'edit';
@@ -15,30 +16,10 @@ export default function PetForm({
   actionType,
   onFormSubmission,
 }: PetFormProps) {
-  const { handleAddPet, selectedPet, handleEditPet } = usePetContext();
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const pet = {
-      name: formData.get('name') as string,
-      ownerName: formData.get('ownerName') as string,
-      imageUrl:
-        (formData.get('imageUrl') as string) ||
-        'https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png',
-      age: +(formData.get('age') as string),
-      notes: formData.get('notes') as string,
-    };
-    if (actionType === 'add') {
-      handleAddPet(pet);
-    } else if (actionType === 'edit') {
-      handleEditPet(selectedPet!.id, pet);
-    }
-    onFormSubmission();
-  };
+  const { selectedPet } = usePetContext();
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col">
+    <form action={addPet} className="flex flex-col">
       <div className="space-y-3">
         <div className="space-y-1">
           <Label htmlFor="name">Name</Label>
