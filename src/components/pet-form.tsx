89c07b2b62b6d37bddc8661e9e0c081e20e00008
KIM YUEN <kim_man@live.com>
2024-10-05 17:56:35 +1000

    feat: close dialog after form submission

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -19,7 +19,13 @@ export default function PetForm({
   const { selectedPet } = usePetContext();
 
   return (
-    <form action={addPet} className="flex flex-col">
+    <form
+      action={async (formData) => {
+        await addPet(formData);
+        onFormSubmission();
+      }}
+      className="flex flex-col"
+    >
       <div className="space-y-3">
         <div className="space-y-1">
           <Label htmlFor="name">Name</Label>
