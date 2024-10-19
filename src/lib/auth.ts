2bb7706ceda515fa11bc4e1893ecd9a29287c4d8
KIM YUEN <kim_man@live.com>
2024-10-19 16:02:14 +1100

    feat: credentials provider in nextauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,37 @@
 import NextAuth, { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import prisma from './db';
+import bcrypt from 'bcryptjs';
 
 const config = {
   pages: {
     signIn: '/login',
   },
-  providers: [],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const { email, password } = credentials;
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+        if (!user) {
+          console.log('User not found');
+          return null;
+        }
+        const passwordsMatched = await bcrypt.compare(
+          password as string,
+          user.hashedPassword
+        );
+        if (!passwordsMatched) {
+          console.log('Invalid credentials');
+          return null;
+        }
+        return user;
+      },
+    }),
+  ],
   callbacks: {
     authorized: ({ request }) => {
       const isTryingToAccessApp = request.nextUrl.pathname.includes('/app');
